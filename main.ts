46f1bc247ae5edcbee279a6f383be523580f2de4
tmheducation <68049755+tmheducation@users.noreply.github.com>
2022-10-10 11:22:51 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,5 @@
-basic.showLeds(`
-    # # # # #
-    # . . . #
-    # # . # #
-    # . # . #
-    # # # # #
-    `)
-basic.showLeds(`
-    # # . # #
-    # . # . #
-    # . . . #
-    . # . # .
-    . . # . .
-    `)
-basic.showString("Hallo")
+basic.showString("Hallo Lars")
+music.playMelody("C D E F G A B C5 ", 120)
 basic.forever(function () {
-    music.playMelody("C D E F G A B C5 ", 120)
+	
 })
